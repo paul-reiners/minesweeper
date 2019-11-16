3a73e97da9e9506b5993e2bdd6b4d6669ec28d16
Paul Reiners <paul.reiners@gmail.com>
2019-11-16 14:26:18 -0600

    "2x2 without bomb" test case done.

--- src/test/scala/org/reiners/triplebyte/StateTest.scala
+++ src/test/scala/org/reiners/triplebyte/StateTest.scala
@@ -2,10 +2,30 @@ package org.reiners.triplebyte
 
 
 class StateTest extends org.scalatest.FunSuite {
+  test("2x2 without bomb") {
+    val n = 2
+    val containsBomb = Array.ofDim[Boolean](n, n)
+    val state = new State(n, containsBomb)
+    val gameOverBefore = state.gameOver()
+    assert(!gameOverBefore)
+    val playerWonBefore = state.playerWon()
+    assert(!playerWonBefore)
+    val playerLostBefore = state.playerLost()
+    assert(!playerLostBefore)
+
+    state.update(0, 0)
+
+    val gameOverAfter = state.gameOver()
+    assert(gameOverAfter)
+    val playerWonAfter = state.playerWon()
+    assert(playerWonAfter)
+    val playerLostAfter = state.playerLost()
+    assert(!playerLostAfter)
+  }
+
   test("1x1 without bomb") {
     val n = 1
     val containsBomb = Array.ofDim[Boolean](n, n)
-    containsBomb(0)(0) = false
     val state = new State(n, containsBomb)
     val gameOverBefore = state.gameOver()
     assert(!gameOverBefore)
